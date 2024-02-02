bbc9c5b3b20bf756ced2edf109ff033df3879fa7
SERRE Aurélien <aurelienserre20@icloud.com>
2024-02-02 08:54:52 +0100

    first ommit

---
+++ swift_ui_unsplash/swift_ui_unsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_unsplashApp.swift
+//  swift_ui_unsplash
+//
+//  Created by SERRE Aurélien on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_unsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
